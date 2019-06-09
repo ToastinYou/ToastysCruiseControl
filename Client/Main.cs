47336ff2f03382800f71aab39b71042e5d89b0f8
ToastinYou <toastinyou@gmx.com>
2019-06-08 23:18:36 -0400

    update to latest citizenfx refs; added disabling of certain veh classes; bug fixes such as accelerating to cruise to a higher speed was fixed.

--- Client/Main.cs
+++ Client/Main.cs
@@ -9,22 +9,19 @@ namespace Client
     public class Main : BaseScript
     {
         private bool _cruising;
+        private float _rpm;
         private readonly Control _cruiseKey;
-        private readonly iniconfig _config = new iniconfig(API.GetCurrentResourceName().ToString(), "Config.ini");
+        private readonly iniconfig _config = new iniconfig(API.GetCurrentResourceName(), "Config.ini");
 
         public Main()
         {
             _cruiseKey = (Control)_config.GetIntValue("keybinds", "toggle", 168);
 
             Debug.WriteLine($"{Common.Prefix} Cruise key: {(int) _cruiseKey}");
-            Debug.WriteLine($"{Common.Prefix} Resource name: {API.GetCurrentResourceName().ToString()}");
-
-            Tick += Process;
+            Debug.WriteLine($"{Common.Prefix} Resource name: {API.GetCurrentResourceName()}");
         }
 
-        /*     
-        
-        List<VehicleClass> vehClassesWithoutCruiseControl = new List<VehicleClass>
+        private List<VehicleClass> _vehClassesWithoutCruiseControl = new List<VehicleClass>
         {
             VehicleClass.Cycles,
             VehicleClass.Motorcycles,
@@ -34,31 +31,34 @@ namespace Client
             VehicleClass.Trains
         };
 
-        */
-
+        [Tick]
         private async Task Process()
         {
-            if (Game.PlayerPed.CurrentVehicle != null)
-            { // In a veh.
-                Vehicle v = Game.PlayerPed.CurrentVehicle;
+            Vehicle v = Game.PlayerPed?.CurrentVehicle;
 
+            if (v != null)
+            {
                 Game.DisableControlThisFrame(0, _cruiseKey);
 
                 if ((Game.IsDisabledControlJustReleased(0, _cruiseKey) || Game.IsControlJustReleased(0, _cruiseKey)) &&
-                    Game.CurrentInputMode == InputMode.MouseAndKeyboard && v.CurrentGear != 0) //vehClassesWithoutCruiseControl.IndexOf(_vehClass) != -1
-                { // Cruise key just released using mouse&keyboard, NOT gamepad.
-
-                    _cruising = !_cruising; // toggle cruising mode.
+                    v.CurrentGear != 0 && !_vehClassesWithoutCruiseControl.Contains(v.ClassType)) // current gear of 0 is reverse.
+                {
+                    _cruising = !_cruising;
 
                     if (_cruising)
                     {
                         CruiseAtSpeed(v.Speed);
+                        _rpm = v.CurrentRPM;
                     }
                 }
             }
+            else if (_cruising)
+            {
+                _cruising = false;
+            }
             else
             { // Not in a veh, check periodically.
-                await Delay(1000);
+                await Delay(100);
             }
         }
 
@@ -66,25 +66,19 @@ namespace Client
         {
             while (_cruising)
             {
-                if (Game.PlayerPed.CurrentVehicle == null)
-                { // Current veh no longer exists, no longer in a veh, stop cruising.
-                    _cruising = false;
-                    break;
-                }
-                else
-                { // Set speed of veh to the cruise speed infinently.
-                    Vehicle v = Game.PlayerPed.CurrentVehicle;
+                Vehicle v = Game.PlayerPed?.CurrentVehicle;
 
-                    v.Speed = s;
-                    v.CurrentRPM = 0.5f;
+                if (v != null)
+                {
+                    v.Speed = s; // try: void SetDriveTaskCruiseSpeed(int /* Ped */ driver, float cruiseSpeed);
+                    v.CurrentRPM = _rpm;
 
-                    if (v.IsInWater || v.IsInBurnout || !v.IsEngineRunning || v.Driver == null ||
-                        v.Driver != Game.PlayerPed ||
+                    if (v.Driver == null || v.Driver != Game.PlayerPed || v.IsInWater || v.IsInBurnout || !v.IsEngineRunning || 
                         v.IsInAir || v.HasCollided ||
                         GTASpeedToMPH(v.Speed) <= 25f || GTASpeedToMPH(v.Speed) >= 100f ||
                         HaveAnyTiresBurst(v) ||
-                        Game.IsControlPressed(0, Control.VehicleHandbrake) ||
-                        Game.IsControlPressed(0, Control.VehicleBrake))
+                        Game.IsControlPressed(0, Control.VehicleHandbrake) || Game.IsDisabledControlPressed(0, Control.VehicleHandbrake) ||
+                        Game.IsControlPressed(0, Control.VehicleBrake) || Game.IsDisabledControlPressed(0, Control.VehicleBrake))
                     { // Disable cruise if any of these.... ^
                         _cruising = false;
                     }
@@ -94,6 +88,10 @@ namespace Client
                     { // Accelerating to new speed..
                         AcceleratingToNewSpeed();
                     }
+                }
+                else
+                {
+                    return;
                 }
 
                 await Delay(0);
@@ -108,7 +106,7 @@ namespace Client
                    Game.IsDisabledControlPressed(0, Control.VehicleAccelerate)) &&
                    Game.PlayerPed.CurrentVehicle != null)
             {
-                await Delay(500); // Wait for client to stop accelerating.
+                await Delay(100); // Wait for client to stop accelerating.
             }
 
             if (Game.PlayerPed.CurrentVehicle != null)
@@ -119,7 +117,7 @@ namespace Client
         }
 
         /// <summary>
-        /// Converts GTA's speeds to MPH.
+        /// Converts GTA's speeds (meters per second) to MPH.
         /// </summary>
         private float GTASpeedToMPH(float s)
         {
